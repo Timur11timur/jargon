b5ef86b5726ad885f05ffd6da9a2e3c7c10aab39
Timur11timur <timur__timur@mail.ru>
2022-02-05 10:05:17 +0200

    Add data provider

--- tests/TagParserTest.php
+++ tests/TagParserTest.php
@@ -7,56 +7,80 @@ use PHPUnit\Framework\TestCase;
 
 class TagParserTest extends TestCase
 {
-    protected TagParser $parser;
+//    protected TagParser $parser;
+//
+//    protected function setUp(): void
+//    {
+//        $this->parser = new TagParser();
+//    }
 
-    protected function setUp(): void
+    /**
+     * @dataProvider tagsProvider
+     */
+    public function test_it_parses__tags($expected, $input)
     {
-        $this->parser = new TagParser();
-    }
+        $parser = new TagParser();
 
-    /** @test */
-    public function it_parses_a_single_tag()
-    {
-        $result = $this->parser->parse('personal');
-        $expected = ['personal'];
-
-        $this->assertSame($expected, $result);
-    }
-
-    /** @test */
-    public function it_parses_a_comma_separated_list_of_tags()
-    {
-        $result = $this->parser->parse('personal, money, family');
-        $expected = ['personal', 'money', 'family'];
+        $result = $parser->parse($input);
 
         $this->assertSame($expected, $result);
     }
 
-    /** @test */
-    public function it_parses_a_pipe_separated_list_of_tags()
+    public function tagsProvider(): array
     {
-        $result = $this->parser->parse('personal | money | family');
-        $expected = ['personal', 'money', 'family'];
-
-        $this->assertSame($expected, $result);
+        return [
+            [['personal'], 'personal'],
+            [['personal', 'money', 'family'], 'personal, money, family'],
+            [['personal', 'money', 'family'], 'personal | money | family'],
+            [['personal', 'money', 'family'], 'personal,money,family'],
+            [['personal', 'money', 'family'], 'personal ,money ,family'],
+            [['personal', 'money', 'family'], 'personal, money ,family'],
+        ];
     }
 
-    /** @test */
-    public function spaces_are_optional()
-    {
-        $result = $this->parser->parse('personal,money,family');
-        $expected = ['personal', 'money', 'family'];
+//    /** @test */
+//    public function it_parses_a_single_tag()
+//    {
+//        $result = $this->parser->parse('personal');
+//        $expected = ['personal'];
+//
+//        $this->assertSame($expected, $result);
+//    }
 
-        $this->assertSame($expected, $result);
+//    /** @test */
+//    public function it_parses_a_comma_separated_list_of_tags()
+//    {
+//        $result = $this->parser->parse('personal, money, family');
+//        $expected = ['personal', 'money', 'family'];
+//
+//        $this->assertSame($expected, $result);
+//    }
 
-        $result = $this->parser->parse('personal ,money ,family');
-        $expected = ['personal', 'money', 'family'];
+//    /** @test */
+//    public function it_parses_a_pipe_separated_list_of_tags()
+//    {
+//        $result = $this->parser->parse('personal | money | family');
+//        $expected = ['personal', 'money', 'family'];
+//
+//        $this->assertSame($expected, $result);
+//    }
 
-        $this->assertSame($expected, $result);
-
-        $result = $this->parser->parse('personal, money ,family');
-        $expected = ['personal', 'money', 'family'];
-
-        $this->assertSame($expected, $result);
-    }
+//    /** @test */
+//    public function spaces_are_optional()
+//    {
+//        $result = $this->parser->parse('personal,money,family');
+//        $expected = ['personal', 'money', 'family'];
+//
+//        $this->assertSame($expected, $result);
+//
+//        $result = $this->parser->parse('personal ,money ,family');
+//        $expected = ['personal', 'money', 'family'];
+//
+//        $this->assertSame($expected, $result);
+//
+//        $result = $this->parser->parse('personal, money ,family');
+//        $expected = ['personal', 'money', 'family'];
+//
+//        $this->assertSame($expected, $result);
+//    }
 }

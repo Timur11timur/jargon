6ce048a8d4fa7af22d9eac0bf58225eaa1c3d703
Timur11timur <timur__timur@mail.ru>
2022-02-17 08:54:50 +0200

    Add isComplete test

--- tests/QuizTest.php
+++ tests/QuizTest.php
@@ -95,4 +95,20 @@ class QuizTest extends TestCase
 
         $this->assertFalse($quiz->nextQuestion());
     }
+
+    /** @test */
+    public function it_knows_if_it_is_complete()
+    {
+        $quiz = new Quiz();
+
+        $quiz->addQuestion(
+            new Question('What is 2 + 2?', 4)
+        );
+
+        $this->assertFalse($quiz->isComplete());
+
+        $quiz->nextQuestion()->answer(4);
+
+        $this->assertTrue($quiz->isComplete());
+    }
 }

79e703e186c179a4b52779cca388e5fe646f363f
Timur <t.fralik@aimprosoft.com>
2023-01-14 19:08:27 +0200

    Create a fake class on a fly

--- tests/SubscriptionTest.php
+++ tests/SubscriptionTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests;
 
+use App\Dummy\Gateway;
 use App\Dummy\Subscription;
 use App\Dummy\User;
 use PHPUnit\Framework\TestCase;
@@ -17,7 +18,9 @@ class SubscriptionTest extends TestCase
     /** @test */
     public function creating_a_subscription_marks_the_user_as_subscribed()
     {
-        $gateway = new FakeGateway();
+        //$gateway = new FakeGateway();
+        $gateway = $this->createMock(Gateway::class);
+
         $subscription = new Subscription($gateway);
         $user = new User('JohnDoe');
 
